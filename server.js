d875977df20a5373325a1de917b8d8d3787f70d3
mcje29zudy <76673962+mcje29zudy@users.noreply.github.com>
2020-12-28 03:52:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to Heroku https://github.com/mcje29zudy/shadowsocks-heroku");
   });
 
   wss = new WebSocketServer({
